b50cd24e86ee8d5145da1159977a1fb54a994cc4
Bruno Gianini <blgianini@gmail.com>
2025-01-23 22:28:23 -0300

    feat: deploy 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send({message: "Olá!"})
+    res.send({message: "Olá!!!!"})
 })
 
 app.listen(8080, () => {
